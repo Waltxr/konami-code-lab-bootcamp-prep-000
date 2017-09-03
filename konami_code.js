32ddfafa879e99c749ba122fb1ee1a6b2c7cadab
Adam Walter <adam.iverson.walter@gmail.com>
2017-09-03 21:35:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0
 
 function init() {
-
   document.body.addEventListener('keydown', function(e) {
 
     const key = parseInt(e.which)
@@ -17,6 +16,6 @@ function init() {
         }
     } else {
       index = 0;
-  }
-})
+    }
+  })
 }
